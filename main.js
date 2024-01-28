33332310366594f70dca96ca85ded727165abd24
diep20 <156032457+diep20@users.noreply.github.com>
2024-01-27 21:59:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+ class Food {
+    constructor(name, calories) {
+        this.name = name;
+        this.calories = parseInt(calories);
+    }
+}
 
+let foodArray = [
+    new Food('Rice', 210),
+    new Food('Raisin', 120)
+];
+  let totalCalories = 0;
+document.getElementById('calorieForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  let calories = parseInt(document.getElementById('calories').value);
+  totalCalories += calories;
+  document.getElementById('totalCalories').innerText = 'Total Calories: ' + totalCalories;
+});
